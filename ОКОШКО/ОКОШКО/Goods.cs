67e96c144e9a69f542d325c94bf89c32e167937f
MikhailMalchik <54844648+MikhailMalchik@users.noreply.github.com>
2020-01-07 02:59:01 +0300

    Add files via upload

---
+++ ОКОШКО/ОКОШКО/Goods.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    interface Goods
+    {
+        string Name { get; set; }
+        int Cost { get; set; }
+        string Type { get; set; }
+        int Size { get; set; }
+    }
+
+}
