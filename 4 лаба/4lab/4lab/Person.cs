be25c867b7c5703c473ef4fd148b009f1783b99e
MikhailMalchik <54844648+MikhailMalchik@users.noreply.github.com>
2020-01-07 02:46:46 +0300

    Add files via upload

---
+++ 4 лаба/4lab/4lab/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Flowers 
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public Flowers(string Type, string Name, int Cost, string Color)
+        {
+            this.Type = Type;
+            this.Name = Name;
+            this.Cost = Cost;
+            this.Color = Color;
+        }
+        public virtual void GetClockInf()
+        {
+            Console.WriteLine($"Name: {Name},Type: {Type}, Cost: {Cost},Color: {Color}");
+        }
+        public override string ToString()
+        {
+            return "Tostring Flowers";
+        }
+    }
+}
