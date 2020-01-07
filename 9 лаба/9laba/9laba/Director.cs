d4993fc597d32bd7941714cdfb8670357c7c677f
MikhailMalchik <54844648+MikhailMalchik@users.noreply.github.com>
2020-01-07 03:09:27 +0300

    Add files via upload

---
+++ 9 лаба/9laba/9laba/Director.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9laba
+{
+    public class Director
+    {
+        delegate int Operation(int x, int y);
+        public delegate void Message(string Message);
+        public event Message Notify2;
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int CareerLevel { get; set; }
+
+        public Director(string Name,int Salary, int CareerLevel)
+        {
+            this.Name = Name;
+            this.Salary = Salary;
+            this.CareerLevel = CareerLevel;
+        }
+        public void AddSalary(int money)
+        {
+            Operation operation = (x, y) => x + y;
+            operation(Salary, money);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Notify2?.Invoke($"Зарплата повышена на {money}$");
+            Console.ResetColor();
+        }
+        public void AddLevel(int level)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Operation operation = (x, y) => x + y;
+            operation(CareerLevel, level);
+            Notify2?.Invoke($"Уровень в карьерной лестнице увеличен {level}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ 9 лаба/9laba/9laba/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace _9laba
+{
+    public delegate void Methods();
+   
+    public delegate void Message(Director person);
+   
+    class Program
+    {
+        public static void Penalty(Director person)
+        {
+            int penalty = person.CareerLevel * 100;
+            person.Salary = person.Salary - penalty;
+        }
+        public static void Promotion(Director person)
+        {
+            int level = 1;
+            int SalaryRaise = 200;
+            person.CareerLevel = person.CareerLevel + level;
+            person.Salary = person.Salary + SalaryRaise;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Message mes;
+            Director Andrey = new Director("Andrey", 2000, 3);
+            Director Alex = new Director("Alex", 1000, 1);
+            Director Sam = new Director("Sam", 1500, 2);
+            Director person = new Director(" ", 0, 0);
+
+            try
+            {
+                Console.WriteLine("Выберите работника(1-3)");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        person = Andrey;
+                        break;
+                    case 2:
+                        person = Alex;
+                        break;
+                    case 3:
+                        person = Sam;
+                        break;
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Ошибка");
+            }
+             
+            
+            Console.WriteLine("Что сделать с работником?");
+            Console.WriteLine("Повысить (1); Оштрафовать(2); Оба события(3)");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    mes = Promotion;
+                    mes(person);
+                    break;
+                case 2:
+                    mes = Penalty;
+                    mes(person);
+                    break;
+                case 3:
+                    mes = Penalty;
+                    mes(person);
+                    mes = Promotion;
+                    mes(person);
+                    break;
+                default: Console.Clear(); break;
+            }
+            Console.WriteLine(person.Name+ ": новая зарплата - " + person.Salary + "$");
+            Console.WriteLine(person.Name + ": новый уровень в карьерной лестнице - " + person.CareerLevel);
+
+            StringMethods test = new StringMethods();
+            test.CurrentString = "Тестовая,   строка,   привет   ААББВВ.";
+            Console.WriteLine(test.CurrentString);
+            Methods Methods = test.DeleteCommas;
+            Methods += test.RemoveSpaces;
+            Methods += test.ToUpperCase;
+            Methods();
+            Console.WriteLine(test.CurrentString);
+
+        }
+    }
+}
